beb8d3e472d26117ce3c9108ea3c3702d7d82bca
M.Ozuna <m.ozuna22@gmail.com>
2024-02-02 16:19:42 -0500

    completed routes

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const Book = require("../models/book");
+const jsonschema = require('jsonschema')
+const booksSchema = require('../schema/books.json')
+const expressError = require('../expressError')
 
 const router = new express.Router();
 
@@ -30,6 +33,11 @@ router.get("/:id", async function (req, res, next) {
 
 router.post("/", async function (req, res, next) {
   try {
+    const result = jsonschema.validate(req.body, booksSchema)
+    if(!result.valid){
+      const listOfErrors = result.errors.map(err => err.stack)
+      throw new expressError(listOfErrors, 400)
+    }
     const book = await Book.create(req.body);
     return res.status(201).json({ book });
   } catch (err) {
@@ -41,6 +49,11 @@ router.post("/", async function (req, res, next) {
 
 router.put("/:isbn", async function (req, res, next) {
   try {
+    const result = jsonschema.validate(req.body, booksSchema)
+    if(!result.valid){
+      const listOfErrors = result.errors.map(err => err.stack)
+      throw new expressError(listOfErrors, 400)
+    }
     const book = await Book.update(req.params.isbn, req.body);
     return res.json({ book });
   } catch (err) {
