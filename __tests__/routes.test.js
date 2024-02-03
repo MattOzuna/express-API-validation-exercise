27ed75e332a29cd9dc0d35eb350dfb12b4a6e490
M.Ozuna <m.ozuna22@gmail.com>
2024-02-02 21:31:18 -0500

    added tests

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -30,9 +30,64 @@ afterAll( async()=>{
 })
 
 describe("GET /books", () => {
-    test('gets a list of all books', async () =>{
+    test('Gets a list of all books', async () =>{
         const response = await request(app).get('/books')
         expect(response.statusCode).toBe(200)
         expect(response.body).toEqual({books: [testBook]})
     })
 })
+
+describe("GET /book/:id", () => {
+    test('Gets a book by id', async () =>{
+        const response = await request(app).get('/books/1234567891')
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual({ book: testBook })
+    })
+})
+
+describe("DELETE /books/:id", () => {
+    test('Deletes a book by id', async () =>{
+        const response = await request(app).delete('/books/1234567891')
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual({ message: "Book deleted" })
+
+        const dbResult = await db.query('SELECT * FROM books')
+        expect(dbResult.rows).toEqual([])
+    })
+})
+
+describe("POST /books", () => {
+    test('Add a new book to the db', async () =>{
+        await db.query('DELETE FROM books')
+        const response = await request(app).post('/books').send(testBook)
+        expect(response.statusCode).toBe(201)
+        expect(response.body).toEqual({book: testBook})
+    })
+})
+
+describe("PUT /books/:id", () => {
+    test('Update a book', async () =>{
+        const response = await request(app).put('/books/1234567891').send({
+            isbn: "1234567891",
+            amazon_url: "http://a.co/eobPtX2",
+            author: "Matthew Lane",
+            language: "english",
+            pages: 264,
+            publisher: "Princeton University Press",
+            title: "Power-Up: Unlocking the Hidden Mathematics in Video Games",
+            year: 2017
+        })
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual({book: {
+            isbn: '1234567891',
+            amazon_url: "http://a.co/eobPtX2",
+            author: "Matthew Lane",
+            language: "english",
+            pages: 264,
+            publisher: "Princeton University Press",
+            title: "Power-Up: Unlocking the Hidden Mathematics in Video Games",
+            year: 2017
+        }})
+    })
+})
+
